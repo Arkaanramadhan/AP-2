98a9e8db06d922ed2730293da546934075616b77
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-04-25 15:35:34 +0700

    Pertemuan 5 : Array & Vector

---
+++ Pertemuan 5/aray.cpp
@@ -0,0 +1,108 @@
+# include <iostream>
+# include <string>
+# include <array>
+using namespace std;
+
+main()
+{
+    // Array Declaration &  Initialization
+    /* 2 cara membuat array */
+
+    // cara  1 :  array kosong
+    // string nama[5];
+    // nama[0] = " Alya ";
+    // nama[1] = " Arkan ";
+    // nama[2] = " Dhafa ";
+    // nama[3] = " Azka ";
+    // nama[4] = " Alamak ";
+
+     // cara  2 :  array yang langsung diisi
+    // string nama[5] = {" alya "," Arkan "," Dhafa "," azka "," Alamak "}
+    // 
+
+    // Ascending Element in Array
+    // cout << nama[0] << endl;
+    // cout << nama[1] << endl;
+    // cout << nama[2] << endl;
+    // cout << nama[3] << endl;
+    // cout << nama[4] << endl;
+
+    //  menggunakan looping
+    // for (int i = 0 ; i <= 4; i++)
+    // {
+    //     cout << nama[i] << endl;
+    // }
+    
+
+    // Multidimensional Array
+    // 1 3 5 
+    // 2 4 6
+
+    // int matrix [2][3] = {{1, 3, 5}, {2, 4, 6}};
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         cout << matrix[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // string (array of characters) // Alya
+    // string nama = "Kiel";
+    // cout << nama[0] << endl;
+    // cout << nama[2] << endl;
+
+    // for (int i  = 0;  i < nama.length(); i++)
+    // {
+    //     cout << nama[i] << endl;
+    // }
+
+    // string s1 = "Hello";
+    // string s2 = "world";
+
+    /*  1. s1 = s2 */
+    // s1 = s2;
+    // cout << "s1 = " << s1 << endl;
+
+    /*  2. s1 = s1 + s2 */
+    // s1 = s1 + s2;
+    // cout << " s1 + s2 = " <<  s1 << endl;
+
+    /*  3. s1.length() */
+    // cout << (s1 + s2).length() << endl;
+
+    /*  4. s1.substr(n,m) */
+    // cout << s1.substr(2,4) << endl;
+    // cout << (s1 + s2).substr(2,4) << endl;
+
+    // Operator sizeof 
+    // int angka = 10;
+    // cout << sizeof(angka) << endl;
+
+    // int nilai[] = {10, 20, 30, 40, 50};
+    // cout << sizeof(nilai) << endl; 
+
+    // string s = "arkaan";
+    // cout << sizeof(s) << endl;
+
+
+
+    // Array library
+    // array<float, 5> nilai = {87.5, 90, 100, 95.7, 78.3};
+    // cout << " Nilai = ";
+
+    // for(int i = 0; i < nilai.size(); i++)
+    // {
+    //     cout << nilai[i] << " ";
+    // }
+
+    // for (float n : nilai)
+    // {
+    //     cout << n << endl;
+    // }
+
+    
+
+ }
+
---
+++ Pertemuan 5/vector.cpp
@@ -0,0 +1,37 @@
+# include <iostream>
+# include <vector>
+using namespace std;
+
+main()
+{
+    // Vector Declaration & Initialization
+    vector<string> nama_karyawan =  {" alya "," Arkan "," Dhafa "," azka "," Alamak "};
+    
+    // ascending  element of Vector
+    // for (int i = 0; i < nama_karyawan.size(); i++)
+    // {
+    //     cout << nama_karyawan[i] << endl;
+    // }
+
+    // for (string karyawan : nama_karyawan)
+    // {
+    //     cout << karyawan << endl;
+    // }
+
+    // Add data to vector
+    // nama_karyawan.push_back("imam");
+    // for (int i = 0; i < nama_karyawan.size(); i++)
+    // {
+    //     cout << nama_karyawan[i] << endl;
+    // }
+
+    // Delete from vector
+    // nama_karyawan.pop_back();
+    // nama_karyawan.erase(nama_karyawan.begin()+3);
+    
+    // for (int i = 0; i < nama_karyawan.size(); i++)
+    // {
+    //     cout << nama_karyawan[i] << endl;
+    // }
+
+}
