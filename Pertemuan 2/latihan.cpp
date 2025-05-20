0018849ce91b6a0ec8c68177a7142d06fa0ac29a
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-20 18:47:41 +0700

    Pertemuan 2 : Penggunaan Operator

--- Pertemuan 2/latihan.cpp
+++ Pertemuan 2/latihan.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
-#include <conio.h> 
-using namespace std;
+#include <iostream> // header untuk C++
+#include <conio.h> // header untuk fungsi getch() dan getche()
+using namespace std; // digunakan agar tidak perlu menulis std :: di depan nama - nama yang berasal dari Standard Library C++ , seperti cin, cout, dll
 
-int main()
+
+int main() // program utama 
 { 
-    float r, volume, lp;
-    const float phi = 3.14; 
+    float r, volume, lp; // variabel r, volume, lp bertipe data float
+    const float phi = 3.14; // memberi nilai variabel phi menggunakan const agar tidak dapat diubah setelah diinisialisasi
 
-    cout << " \n Masukkan jari-jari =  ";
-    cin >> r;
+    cout << " \n Masukkan jari-jari =  "; // menamilkan kalimat " Masukkan jari-jari : "
+    cin >> r; // fungsi inputan pada variabel r
 
-    volume = ((float)4/3) * phi * r * r * r ;
-    lp = 4 * phi * r * r;
+    // (float) 4/3 digunakan agar hasil perhitungan 4/3 dalam bentuk tipe float
+    volume = ((float)4/3) * phi * r * r * r ; // variabel volume menyimpan hasil perhitungan rumus volume bola
+    lp = 4 * phi * r * r; // variabel lp menyimpan hasil perhitungan rumus luas permukaan bola
 
-    cout << " \nVolume = " << volume;
-    cout << " \nLuas Permukaan = " << lp;
+    cout << " \nVolume = " << volume; // menampilkan nilai variabel volume
+    cout << " \nLuas Permukaan = " << lp; // menampilkan nilai variabel lp
 
 
 
