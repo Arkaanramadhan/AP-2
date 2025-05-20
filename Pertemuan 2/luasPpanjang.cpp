17c5c9443ae9c10f3ef58bf2fbf87e8e9022321d
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-20 18:54:55 +0700

    Pertemuan 2 : Penggunaan Operator

--- Pertemuan 2/luasPpanjang.cpp
+++ Pertemuan 2/luasPpanjang.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-#include <conio.h> 
-using namespace std;
+#include <iostream> // header untuk C++
+#include <conio.h> // header untuk fungsi getch() dan getche()
+using namespace std; // digunakan agar tidak perlu menulis std :: di depan nama - nama yang berasal dari Standard Library C++ , seperti cin, cout, dll
 
-int main()
+int main() // program utama
 { 
-    float Luas, p, l;
-    system ("CLS");
+    float Luas, p, l; // variabel Luas, p, l bertipe data float
+    system ("CLS"); // digunakan untuk membersihkan layar pada output agar rapi
 
-    cout << " \n Masukkan Panjang =  ";
-    cin >> p;
+    cout << " \n Masukkan Panjang =  "; // menampilkan kalimat " Masukkan panjang = "
+    cin >> p; // fungsi inputan disimpan pada variabel p
 
-    cout << " \n Masukkan lebar =  ";
-    cin >> l;
+    cout << " \n Masukkan lebar =  "; // menampilkan kalimat " Masukkan lebar = "
+    cin >> l; // fungsi inputan disimpan pada variabel l
 
-    Luas = p * l;
+    Luas = p * l; // variabel Luas menyimpan hasil perhitungan perkalian antara nilai variabel p dan l
 
-    cout << " \nLuas = " << Luas;
+    cout << " \nLuas = " << Luas; // menampilkan nilai dari variabel Luas
 
 
 
