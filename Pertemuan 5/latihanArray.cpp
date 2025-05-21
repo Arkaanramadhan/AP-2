a7ccb039ed61b7e9f6e0123c4eee957f634ed84b
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-21 20:31:57 +0700

    Pertemuan 5 : Array & Vector

--- Pertemuan 5/latihanArray.cpp
+++ Pertemuan 5/latihanArray.cpp
@@ -1,43 +1,44 @@
-# include <iostream>
-using namespace std;
-main()
+# include <iostream> // header pada C++
+using namespace std; //  digunakan agar tidak perlu lagi menulis std :: pada setiap instruksi program
+
+main() // program utama
 {
     // Penjumlahan matriks 2 x 2
 
-    int matriks1[2][2];
-    int matriks2[2][2];
-    int hasil[2][2];
+    int matriks1[2][2]; // variabel matriks1 brukuran 2 x 2 bertipe data integer
+    int matriks2[2][2]; // variabel matriks2 brukuran 2 x 2 bertipe data integer
+    int hasil[2][2]; // variabel hasil1 brukuran 2 x 2 bertipe data integer
 
-    cout << " Matriks 1 : " << endl;
+    cout << " Matriks 1 : " << endl; // data  nilai pada variabel matriks1
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 2; j++)
         {
             cout << "Masukkan elemen baris " << i + 1 << " Kolom "<< j + 1 << " : ";
-            cin >> matriks1[i][j];
+            cin >> matriks1[i][j]; // user menginput nilai angka pada baris ke 1 kolom ke 1 hingga baris 2 kolom 2
         }
 
     }
 
-    cout << " Matriks 2 : " << endl;
+    cout << " Matriks 2 : " << endl; // data  nilai pada variabel matriks2
     for (int i = 0; i < 2;i++)
     {
         for (int j = 0; j < 2; j++)
         {
             cout << "Masukkan elemen baris " << i + 1 << " Kolom "<< j + 1 << " : ";
-            cin >> matriks2[i][j];
+            cin >> matriks2[i][j]; // user menginput nilai angka pada baris ke 1 kolom ke 1 hingga baris 2 kolom 2
         }
         
     }
 
-    cout << " Hasil Penjumlahan " << endl;
+    cout << " Hasil Penjumlahan " << endl; // data hasil penjumlahan anggota matriks1 dan matriks2
     for (int i = 0; i < 2;i++)
     {
         for (int j = 0; j < 2; j++)
         {
-            hasil[i][j] = matriks1[i][j] + matriks2[i][j];
-            cout << hasil[i][j] << " ";
+            hasil[i][j] = matriks1[i][j] + matriks2[i][j]; // menjumlahkan hasil inputan matriks1 dan matriks2
+            cout << hasil[i][j] << " "; // menampilkan hasil penjumlahan matriks1 dan matriks2 dalam bentuk matriks dimulai dari data pada baris ke 1 kolom ke 1 hingga baris 2 kolom 2
         }
-        cout << endl;
+        cout << endl; // agar dapat membentuk seperti tampilan matriks pada umummnya
     }
 }
