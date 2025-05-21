f05fff8ea115bf2f03babb796f1f914abeb33930
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-21 07:58:41 +0700

    Pertemuan 3 : Operasi Kondisi

--- Pertemuan 3/operasiKondisi.cpp
+++ Pertemuan 3/operasiKondisi.cpp
@@ -1,15 +1,15 @@
-# include <iostream>
-using namespace std;
+# include <iostream> // header pada C++
+using namespace std; // // digunakan agar tidak perlu menulis std :: di depan nama - nama yang berasal dari Standard Library C++ , seperti cin, cout, dll
 
-main()
+main() // program utama
 {
-    int nilai;
-    system("CLS");
+    int nilai; // varibel nilai bertipe data integer
+    system("CLS"); // digunakan untuk membersihkan layar pada output agar rapi
 
-    cout << "Masukkan Nilai : ";
-    cin >> nilai;
+    cout << "Masukkan Nilai : "; // fungsi keluaran atau ouput
+    cin >> nilai; // fungsi masukkan atau inputan pada variabel nilai
 
-    // if statement
+    // if statement, dipakai untuk satu kondisi saja, apabila di luar kondisi tersebut maka tidak menjalankan program apapun
 
     // if (nilai <= 65)
     // {
@@ -17,7 +17,7 @@ main()
     // }
 
 
-    // if -  else staement
+    // if -  else staement, dipakai untuk satu kondisi saja, apabila di luar kondisi tersebut maka akan menjalankan program tertentu
 
     // if (nilai <= 65)
     // {
@@ -28,7 +28,7 @@ main()
     //     cout << "Anda Lulus";
     // }
 
-    // if - else - if statemnet
+    // if - else - if statemnet, diapakai untuk lebih dari satu kondisi
 
     // if (nilai == 100)
     // {
@@ -43,7 +43,7 @@ main()
     //     cout << "anda lulus";
     // }
 
-    // nested if
+    // nested if , merupakan kondisi atau syarat di dalam kondisi atau syarat tertentu
 
     // if (nilai <= 65)
     // {
@@ -61,11 +61,12 @@ main()
     //     }
     // }
 
-    // switch case
-
+    // switch case, digunakan untuk program yang membutuhkan banyak kondisi ,tetapi dalam bentuk yang lebih singkat dan efisien.
+    // swtich case dapat berupa nilai(integer) atau string
+    
     // switch (nilai)
     // {
-    //     case 1 : cout << "senin";break;
+    //     case 1 : cout << "senin";break; // diakhiri dengan break agar melanjutkan program yang berada di setelah switch case ini
     //     case 2 : cout << "selasa";break;
     //     case 3 : cout << "rabu";break;
     //     case 4 : cout << "kamis";break;
@@ -76,10 +77,12 @@ main()
 
     // }
 
-    // switch range
+    // switch range, digunakan untuk program yang membutuhkan banyak kondisi ,tetapi dalam bentuk yang lebih singkat dan efisien.
+    // switch range dalam bbentuk range atau rentang suatu angka atau nilai
+    
     // switch (nilai)
     // {
-    //     case 85 ... 100 : cout << " A "; break;
+    //     case 85 ... 100 : cout << " A "; break; // diakhiri dengan break agar melanjutkan program yang berada di setelah switch range ini
     //     case 80 ... 84 : cout << " B+ "; break;
     //     case 75 ... 79 : cout << " B "; break;
     //     case 70 ... 74 : cout << " C+ "; break;
@@ -88,8 +91,9 @@ main()
     //     default : cout << " E "; break;
     // }
     
-    // Ternary Operator
+    // Ternary Operator, operator kondisional untuk mengevaluasi sebuah kondisi dan mengembalikan salah satu dari dua nilai berdasarkan hasil evaluasi
 
-    string checknum = (nilai % 2 == 0) ? "Genap" : "Ganjil";
+    string checknum = (nilai % 2 == 0) ? "Genap" : "Ganjil"; // apabila hasil dari nilai mod 2 == 0 true, maka variabel checknum akan menyimpan string genap
+    // apabila hasil dari nilai mod 2 == 0 false, maka variabel checknum akan menyimpan string ganjil
     cout << nilai << " tuh bilangan " << checknum << " sih " << endl;
 }
