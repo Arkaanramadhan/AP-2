b2caacb21420f11b5393f2a5c039b955a7e7ffaf
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-21 07:35:20 +0700

    Pertemuan 3 : Operasi Kondisi

--- Pertemuan 3/latihan.cpp
+++ Pertemuan 3/latihan.cpp
@@ -1,25 +1,25 @@
-# include <iostream>
-using namespace std;
+# include <iostream> // Header pada C++
+using namespace std; // // digunakan agar tidak perlu menulis std :: di depan nama - nama yang berasal dari Standard Library C++ , seperti cin, cout, dll
 
-main()
+main() // program utama
 {
-    int nilai;
-    system("CLS");
+    int nilai; // variabel nilai bertipe data integer
+    system("CLS"); // digunakan untukmembersihkan layar pada output agar tidak berantakan atau rapi
 
-    cout << "Masukkan Nilai : ";
-    cin >> nilai;
+    cout << "Masukkan Nilai : "; // menampilkan kalimat " Masukkan Nilai : "
+    cin >> nilai; // fungsi masukan atau inputan yang disimpan pada variabel nilai
 
-    if (nilai % 5 == 0)
+    if (nilai % 5 == 0) // syarat atau kondisi apabila hasil dari inputan variabel nilai dimodkan dengan 5 menghasilkan 0
     {
-        cout << nilai << " kelipatan 5";
+        cout << nilai << " kelipatan 5"; // jika memenuhi syarat maka akan menampilkan nilai variabel nilai kelipatan 5
     }
-    else if (nilai % 10 == 0)
+    else if (nilai % 10 == 0) // syarat atau kondisi apabila hasil dari inputan variabel nilai dimodkan dengan 10 menghasilkan 0
     {
-        cout << nilai << " kelipatan 10";
+        cout << nilai << " kelipatan 10"; // jika memenuhi syarat maka akan menampilkan nilai variabel nilai kelipatan 10
     }
-    else 
+    else // kondisi selain dari kondisi atau syarat yang ada di atas
     {
-        cout << nilai << " Bukan Kelipatan 5 atau 10";
+        cout << nilai << " Bukan Kelipatan 5 atau 10"; // jika  memnuhi kondisi tersebut makan akan menampilkan nilai variabel nilai bukan kelipatan 5 atau 10
     }
 
 }
