4677bb985ac753309471c747dbd231d5cc9879a0
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-21 23:13:25 +0700

    Pertemuan 7 : Pointer

--- Pertemuan 7/pointer.cpp
+++ Pertemuan 7/pointer.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 
 
+    // ringkasan saya
+    // pointer -> int *ayam = 20 
+    // ketika dipanggil dalam bentuk variabel ayam saja, maka output akan menghasilkan alamat dari angka 20.
+    // namun ketika dipanggil dalam bentuk *ayam, maka output akan menghasilkan 20.
+
+    // data biasa -> int ayam = 20
+    // ketika dipanggil dalam bentuk variabel ayam saja, maka output akan menghasilkan 20.
+    // namun ketika dipanggil dalam bentuk &ayam, maka output akan menghasilkan alamat dari angka 20.
+
 void penjumlahan(int a, int b)
 {
     cout << a + b << endl;
 }
 
-void penjumlahanPointer(int *a, int *b)
+void penjumlahanPointer(int *a, int *b) // parameter fapat berupa  pointer 
 {
     *a += *b;
     cout << *a << endl;
@@ -36,7 +45,7 @@ main()
     // cout  << " isi variabel yang ditunjuk oleh pointer_number = " << *pointer_number << endl;
     // cout << " Alamat memori variabel pointer_number = " << &pointer_number << endl;
 
-    // pointer in array
+    // pointer in array, pointer dapat digunakan untuk menunjuk alamat atau index data dalam array
     int num[] = {1,2,3,4,5};
     int *pointer_num = num;
     // cout << " Isi variabel num = " << num[0] << endl;
@@ -45,14 +54,14 @@ main()
     // cout  << " isi variabel yang ditunjuk oleh pointer_num = " << *pointer_num << endl;
     // cout << " Alamat memori variabel pointer_num = " << &pointer_num << endl;
 
-    // *pointer_num += 5;
+    // *pointer_num += 5; // pointer juga dapat melakukan operasi baik itu alamat memori maupun isi alamat tersebut
     // cout << " Isi variabel num = " << num << endl;
     // cout << " Alamat dari memori variabel num = " << &num << endl;
     // cout << " isi variabel pointer_num = " << pointer_num << endl;
     // cout  << " isi variabel yang ditunjuk oleh pointer_num = " << *pointer_num << endl;
     // cout << " Alamat memori variabel pointer_num = " << &pointer_num << endl;
 
-    // pointer in parameter 
+    // pointer in parameter , pointer dapat menjadi parameter formal maupun aktual
     // int num1 = 5;
     // int num2 = 7;
     // penjumlahan(num1,num2);
@@ -62,7 +71,7 @@ main()
     // cout << num1 << endl;
     // cout << num2 << endl;
     
-    // pointer in pointer 
+    // pointer in pointer, pointer yang menyimpan alamat dari pointer lain 
     // int score = 4;
     // int *pointer_score = &score;
     // int **ptr_pointer_score = &pointer_score; // pointer in pointer
@@ -70,11 +79,12 @@ main()
     // cout << " Isi variabel pointer_score  = " << pointer_score << " dan isi variabel yang ditunjuk oleh pointer_score = " << *pointer_score << " alamat memori pointer_score = " << &pointer_score << endl;
     // cout << " Isi variabel ptr_pointer_score  = " << ptr_pointer_score << " dan isi variabel yang ditunjuk oleh ptr_pointer_score = " << **ptr_pointer_score << " alamat memori ptr_pointer_score = " << &ptr_pointer_score << endl;
 
-    // Dynamic Pointer 
+    // Dynamic Pointer, pointer digunakan untuk mengakses memori yang dialokasikan secara dinamis
     int *ptr = new int;
     *ptr = 30;
     cout << " Isi variabel ptr " << *ptr << " dan alamat " << &ptr << endl;
 
     delete ptr;
-    cout << " Isi variabel ptr " << *ptr << " dan alamat " << &ptr << endl; 
+    cout << " Isi variabel ptr " << *ptr << " dan alamat " << &ptr << endl;
+
 }
