1fdaa005a0c12760178ef5d88ce5bbe6fceb6d64
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-21 20:59:01 +0700

    Pertemuan 6 : Fungsi & Header

--- Pertemuan 6/belajarFungsi.cpp
+++ Pertemuan 6/belajarFungsi.cpp
@@ -1,13 +1,14 @@
-# include <iostream>
-using namespace std;
+# include <iostream> // header pada C++
+using namespace std;  //  digunakan agar tidak perlu lagi menulis std :: pada setiap instruksi program
 
-void sapa(string nama)// nama sebagai Parameter Formal -> digunakan pada saat Fungsi dibuat
+// fungsi sapa merupakan fungsi tanpa nilai  balikan
+void sapa(string nama)// nama sebagai Parameter Formal -> dibuat pada saat Fungsi dibuat
 {
-    cout <<  " Hallo " << nama << " Selamat Datang di AP 2! " << endl;
+    cout <<  " Hallo " << nama << " Selamat Datang di AP 2! " << endl; // akan menampilkan output Hallo Arkan Selamat Datang di AP 2!
 }
 main()
 {
     string namaPengguna = " Arkan ";
-    sapa(namaPengguna); // namaPengguna sebagai Parameter Aktual -> dibuat pada saat pemanggilan Fungsi
-    return 0;
+    sapa(namaPengguna); // namaPengguna sebagai Parameter Aktual -> dibuat pada saat pemanggilan Fungsi, nama parameter aktual akan berubah menjadi nama parameter formal ketika masuk ke fungsi
+    return 0; // digunakan untuk mengakhiri program suatu fungsi
 }
