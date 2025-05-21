fb67a3f858284ce0673886e9ce363072394ebdec
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-21 21:04:37 +0700

    Pertemuan 6 : Fungsi & Header

--- Pertemuan 6/variabel.cpp
+++ Pertemuan 6/variabel.cpp
@@ -1,21 +1,21 @@
-# include <iostream>
-using namespace std;
+# include <iostream> // Header pada C++
+using namespace std; // digunakan agar tidak perlu lagi menulis std :: pada setiap instruksi program
 
 string namaGlobal = "Ilmu";// namaGlobal -> variabel global
 void namaVariabel()
 {
     string namalokal = "komputer";// namalokal -> variabel lokal
     // coba akses
-    cout << namalokal << endl;
+    cout << namalokal << endl; // menampilkan komputer
     // coba akses
-    cout << namaGlobal << endl;
+    cout << namaGlobal << endl; // menampilkan Ilmu
 }
 
 main()
 {
     namaVariabel();
     // coba akses
-    // cout << namaGlobal << endl;
+    // cout << namaGlobal << endl; // variabel global dapat diakses dari luar maupun dari dalam fungsi sehingga akan menampilkan keluaran Ilmu
     // coba akses
-    // cout << namalokal << endl; //ga iso
+    // cout << namalokal << endl; // variabel lokal hanya dapat diakses dari dalam fungsi sehingga baris ini tidak menampilkan ouput atau tidak bisa
 }
