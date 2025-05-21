b3be4d5b591fc93fb733105a21b65e4eb1ef2037
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-21 16:40:26 +0700

    Pertemuan 4 : Perulangan

--- Pertemuan 4/latihan2.cpp
+++ Pertemuan 4/latihan2.cpp
@@ -1,22 +1,28 @@
-# include <iostream>
-using namespace std;
+# include <iostream> // Header pada C++
+using namespace std; // digunakan agar tidak perlu menulis std :: di depan nama - nama yang berasal dari Standard Library C++ , seperti cin, cout, dll
 
-int main(){
-    string kalimat;
-    int i;
 
-    system("CLS");
+int main() // program Utama
+{
+    string kalimat; // variabel kalimat bertipe data string
+    int i; // variabel i bertipe data integer
 
-    cout << "Masukkan kalimat : "; getline(cin, kalimat);
+    system("CLS"); // digunakan untuk membersihkan layar pada output agar tidak berantakan
 
-    for (i = 0; i < kalimat.length(); i++) {
-        if (kalimat[i] == 'a'|| kalimat[i] == 'e' || kalimat[i] == 'i' || kalimat[i] == 'o' || kalimat[i] == 'u')
+    cout << "Masukkan kalimat : "; // menampilkan kalimat " Masukkan Kalimat : "
+    getline(cin, kalimat); // fungsi masukkan atau inputan dengan spasi dan disimpan pada variabel kalimat
+
+    for (i = 0; i < kalimat.length(); i++) // perulangan akan berhenti ketika i >= panjang kalimat yang kita input pada variabel kalimat atau kalimat.length()
+    // i akan bertambah 1 setiap perulangan
+        
+    {
+        if (kalimat[i] == 'a'|| kalimat[i] == 'e' || kalimat[i] == 'i' || kalimat[i] == 'o' || kalimat[i] == 'u') // kondisi jika ada huruf kecil a, i, i, o, u pada kalimat yang diinput user atau pada index panjang kalimat
         {
-            kalimat[i] = toupper(kalimat[i]);
+            kalimat[i] = toupper(kalimat[i]); // membuat huruf kecil a, e, i, o, u menjadi huruf kapital
         }
     }
 
-    cout << "Kalimat dalam huruf kapital : "  << kalimat << endl;
+    cout << "Kalimat dalam huruf kapital : "  << kalimat << endl; // menampilkan isi variabel kalimat yang sudah dikapitalkan huruf kecil a, e, i, o, u (jika ada pada inputan user)
 
-    return 0;
+    return 0; // mengakhiri eksekusi sebuah fungsi
 }
