295ee430861f08a2bc5b4f1f363688d854733787
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-21 20:35:05 +0700

    Pertemuan 5 : Array & Vector

--- Pertemuan 5/latihanArray.cpp
+++ Pertemuan 5/latihanArray.cpp
@@ -10,9 +10,9 @@ main() // program utama
     int hasil[2][2]; // variabel hasil1 brukuran 2 x 2 bertipe data integer
 
     cout << " Matriks 1 : " << endl; // data  nilai pada variabel matriks1
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++) // mengatur bagian baris
     {
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < 2; j++) // mengatur bagian kolom
         {
             cout << "Masukkan elemen baris " << i + 1 << " Kolom "<< j + 1 << " : ";
             cin >> matriks1[i][j]; // user menginput nilai angka pada baris ke 1 kolom ke 1 hingga baris 2 kolom 2
@@ -21,9 +21,9 @@ main() // program utama
     }
 
     cout << " Matriks 2 : " << endl; // data  nilai pada variabel matriks2
-    for (int i = 0; i < 2;i++)
+    for (int i = 0; i < 2;i++) // mengatur bagian baris
     {
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < 2; j++) // mengatur bagian kolom
         {
             cout << "Masukkan elemen baris " << i + 1 << " Kolom "<< j + 1 << " : ";
             cin >> matriks2[i][j]; // user menginput nilai angka pada baris ke 1 kolom ke 1 hingga baris 2 kolom 2
@@ -32,9 +32,9 @@ main() // program utama
     }
 
     cout << " Hasil Penjumlahan " << endl; // data hasil penjumlahan anggota matriks1 dan matriks2
-    for (int i = 0; i < 2;i++)
+    for (int i = 0; i < 2;i++) // mengatur bagian baris
     {
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < 2; j++) // mengatur bagian kolom
         {
             hasil[i][j] = matriks1[i][j] + matriks2[i][j]; // menjumlahkan hasil inputan matriks1 dan matriks2
             cout << hasil[i][j] << " "; // menampilkan hasil penjumlahan matriks1 dan matriks2 dalam bentuk matriks dimulai dari data pada baris ke 1 kolom ke 1 hingga baris 2 kolom 2
