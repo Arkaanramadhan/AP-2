1255b8fa326ad8355722c4f0bbe6bac0931d47a3
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-21 21:07:27 +0700

    Pertemuan 6 : Fungsi & Header

--- Pertemuan 6/headerCustom.h
+++ Pertemuan 6/headerCustom.h
@@ -1,4 +1,4 @@
-#define l <<
-#define u using namespace std;
-#define c cout 
-#define awal main()
+#define l << // memanipulasi atau mengubah sintaks << menjadi l
+#define u using namespace std; // memanipulasi atau mengubah sintaks using namespace std menjadi u
+#define c cout // memanipulasi atau mengubah sintaks cout menjadi c
+#define awal main() // memanipulasi atau mengubah sintaks main() menjadi awal
