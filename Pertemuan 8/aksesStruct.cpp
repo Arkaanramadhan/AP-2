dfc07ef011fcdb8f5eb62ecc2129629a1321642e
Arkaanramadhan <arkaan06rusdi@gmail.com>
2025-05-21 22:23:56 +0700

    Pertemuan 8 : Struct & Class

--- Pertemuan 8/aksesStruct.cpp
+++ Pertemuan 8/aksesStruct.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //  Header pada C++
+using namespace std; // digunakan agar tidak perlu lagi membuat std :: pada tiap instruksi program
 
-struct Mahasiswa
+struct Mahasiswa // struct fungsinya sama seperti array, tetapi struct dapat menyimpan beberapa data dengan tipe data yang berbeda, dan struct ini bernama Mahasiswa
 {
-    string nama;
-    int umur;
-    float ipk;
-};
+    string nama; // variabel nama bertipe data string
+    int umur; // variabel umur bertipe data integer
+    float ipk; // varibel ipk bertipe dta float
+}; // pada akhir struct harus menggunakan ;
 
-main()
+main() // program utama
 {
-    system("CLS");
-    Mahasiswa mhs1;
+    system("CLS"); // digunakan untuk membersihkan layar pada output
+    Mahasiswa mhs1; // membuat variabel mhs1 menjadi objek baru dari struct Mahasiswa
 
-    mhs1.nama = " Arkaan ";
-    mhs1.umur = 20;
-    mhs1.ipk = 4.00;
+    mhs1.nama = " Arkaan "; // sehingga pemanggilan menggunakan objek atau mhs1.nama_variabel_dlm_struct
+    mhs1.umur = 20; //umur bertipe data integer pada struct
+    mhs1.ipk = 4.00; // ipk bertipe data float pada struct
 
-    // cout << " nama = " << mhs1.nama  << endl;
-    // cout << " umur = " << mhs1.umur  << endl;
-    // cout << " ipk = " << mhs1.ipk  << endl;
+    // cout << " nama = " << mhs1.nama  << endl; // akan menampilkan nama = Arkaan
+    // cout << " umur = " << mhs1.umur  << endl; // akan menampilkan umur = 20
+    // cout << " ipk = " << mhs1.ipk  << endl;// akan menampilkan ipk = 4.00
 
-    Mahasiswa *ptrMhs = &mhs1;
-    cout << " Nama = " << ptrMhs->nama << endl;
-    cout << " Umur = " << ptrMhs->umur << endl;
-     cout << " ipk = " << ptrMhs->ipk << endl;
+    Mahasiswa *ptrMhs = &mhs1; // ptMhs akan menunjuk isi dari alamat yang ada pada mhs1
+    cout << " Nama = " << ptrMhs->nama << endl; // pendeklarasian nya ptrMhs ->nama_variabel_dlm_struct
+    cout << " Umur = " << ptrMhs->umur << endl; // akan menampilkan  umur = 20
+     cout << " ipk = " << ptrMhs->ipk << endl; // akan menampilkan ipk =4.00
     
     
 }
